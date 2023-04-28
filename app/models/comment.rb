50e411c593246129f6f9d1fa2515c86a48a8a1c1
chrisbanas <bananas595@gmail.com>
2023-04-28 13:39:01 -0700

    validations done

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -20,8 +20,19 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: :User
   belongs_to :post, class_name: :Post
 
-  # is this right?
-  belongs_to :parent_comment, class_name: :Comment
+  belongs_to :parent_comment,
+  foreign_key: :parent_comment_id,
+  class_name: :Comment,
+  optional: true
+
+  has_many :replies,
+  foreign_key: :parent_comment_id,
+  class_name: :Comment,
+  dependent: :destroy
+
+  has_one :parent_commenter,
+  through: :parent_comment,
+  source: :commenter
 
   has_many :likes, as: :likeable, dependent: :destroy
 
