3964796c7388e4c51cf9cbdccdf098a798de42de
chrisbanas <bananas595@gmail.com>
2023-04-30 21:04:00 -0700

    aws profile image works

--- frontend/src/components/NewsFeed/PersonalFeed/Post/PostOwnerInfo/PostOwnerInfo.js
+++ frontend/src/components/NewsFeed/PersonalFeed/Post/PostOwnerInfo/PostOwnerInfo.js
@@ -14,7 +14,7 @@ export default function PostOwnerInfo({ post }) {
             <div className="post-owner-avatar-container">
               <span className="post-news-feed-owner-avatar">
                 <div className="post-news-feed-owner-avatar-image">
-                  <img className="news-feed-user-avatar-image" alt="user avatar" data-pin-nopin="true" src="https://us1-photo.nextdoor.com/user_photos/33/7d/337d37645b9f50c6c07e2b6f6fa73fe8.jpg?request_version=v2&output_type=jpg&sizing=linear&x_size=1&resize_type=resize" />
+                  <img className="news-feed-user-avatar-image" alt="user avatar" data-pin-nopin="true" src={post.userPhoto} />
                 </div>
               </span>
             </div>
