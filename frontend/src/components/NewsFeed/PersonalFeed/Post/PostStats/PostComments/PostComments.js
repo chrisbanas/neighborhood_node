2c534e8e1b6cca601901a4d23583c25ed172b147
chrisbanas <bananas595@gmail.com>
2023-05-01 17:18:26 -0700

    post comment stats templete implemented

--- frontend/src/components/NewsFeed/PersonalFeed/Post/PostStats/PostComments/PostComments.js
+++ frontend/src/components/NewsFeed/PersonalFeed/Post/PostStats/PostComments/PostComments.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { deletePost } from "../../../../../../store/posts";
+import PostCommentsStats from "./PostCommentsStats/PostCommentsStats";
 import "./PostComments.css";
 
 export default function PostComments({ post }) {
@@ -40,69 +41,69 @@ export default function PostComments({ post }) {
     <>
 
 
-        <div className="child-news-feed-comment-user-info-container">
-          <div className="grandchild-news-feed-comment-user-info-container">
-            {/* <!-- user avatar --> */}
-            <div className="comment-owner-avatar-container" onMouseEnter={handleMouseEnter}
-              onMouseLeave={handleMouseLeave}>
-              <span className="comment-news-feed-owner-avatar" >
-                <div className="comment-news-feed-owner-avatar-image">
-                  <img
-                    className="news-feed-user-avatar-image" alt="user avatar" src={post.userPhoto} />
-                </div>
-              </span>
-            </div>
-            {/* <!-- user info --> */}
-              <div className="news-feed-comment-owner-name-container">
-                <a className="news-feed-comment-owner-name" href="/news_feed">
-                  {post.authorFirstName}&nbsp;{post.authorLastName}
-                </a>
+      <div className="child-news-feed-comment-user-info-container">
+        <div className="grandchild-news-feed-comment-user-info-container">
+          {/* <!-- user avatar --> */}
+          <div className="comment-owner-avatar-container" onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}>
+            <span className="comment-news-feed-owner-avatar" >
+              <div className="comment-news-feed-owner-avatar-image">
+                <img
+                  className="news-feed-user-avatar-image" alt="user avatar" src={post.userPhoto} />
               </div>
-              <div className="news-feed-comment-neighborhood-name-container">
-                <a className="news-feed-comment-neighborhood-name" href="/news_feed">
-                  {post.neighborhoodName}
-                </a>
-              </div>
-            {/* <!-- popup --> */}
-            {showPopup && (
-              <div className="news-feed-comment-user-profile-popup">
-                <div className="sub-news-feed-comment-user-profile-popup">
-                  <img className="popup-news-feed-user-avatar-image" alt="user avatar" src={post.userPhoto} />
+            </span>
+          </div>
+          {/* <!-- user info --> */}
+          <div className="news-feed-comment-owner-name-container">
+            <a className="news-feed-comment-owner-name" href="/news_feed">
+              {post.authorFirstName}&nbsp;{post.authorLastName}
+            </a>
+          </div>
+          <div className="news-feed-comment-neighborhood-name-container">
+            <a className="news-feed-comment-neighborhood-name" href="/news_feed">
+              {post.neighborhoodName}
+            </a>
+          </div>
+          {/* <!-- popup --> */}
+          {showPopup && (
+            <div className="news-feed-comment-user-profile-popup">
+              <div className="sub-news-feed-comment-user-profile-popup">
+                <img className="popup-news-feed-user-avatar-image" alt="user avatar" src={post.userPhoto} />
+                <div>
+                  <div className="popup-news-feed-comment-neighborhood-name">
+                    {post.authorFirstName}&nbsp;{post.authorLastName}
+                  </div>
                   <div>
-                    <div className="popup-news-feed-comment-neighborhood-name">
-                      {post.authorFirstName}&nbsp;{post.authorLastName}
-                    </div>
-                    <div>
-                      {post.neighborhoodName}
-                    </div>
+                    {post.neighborhoodName}
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
+        {/* <!-- Edit / delete dropdown --> */}
+        <div className="news-feed-comment-delete-edit-dropdown-container">
+          <div className="sub-news-feed-comment-delete-edit-dropdown-container" onClick={handleDropdownClick}>
+            <svg className="news-feed-comment-delete-edit-dropdown-icon" width="24" height="24" viewBox="0 0 24 24" role="img">
+              <path fill="currentColor" d="M5.5 14a2 2 0 1 0 0-4 2 2 0 0 0 0 4ZM12 14a2 2 0 1 0 0-4 2 2 0 0 0 0 4ZM20.5 12a2 2 0 1 1-4 0 2 2 0 0 1 4 0Z"></path>
+            </svg>
+            {dropdownVisible && (
+              <div className="news-feed-comment-delete-edit-dropdown-menu-container">
+                <div className="news-feed-comment-delete-edit-dropdown-menu">
+                  <div className="news-feed-comment-delete-edit-dropdown-item" onClick={handleEditClick}>
+                    Edit Comment
+                  </div>
+                  <div className="news-feed-comment-delete-edit-dropdown-item" onClick={handleDeleteClick}>
+                    Delete Comment
                   </div>
                 </div>
               </div>
             )}
           </div>
-          {/* <!-- Edit / delete dropdown --> */}
-          <div className="news-feed-comment-delete-edit-dropdown-container">
-            <div className="sub-news-feed-comment-delete-edit-dropdown-container" onClick={handleDropdownClick}>
-              <svg className="news-feed-comment-delete-edit-dropdown-icon" width="24" height="24" viewBox="0 0 24 24" role="img">
-                <path fill="currentColor" d="M5.5 14a2 2 0 1 0 0-4 2 2 0 0 0 0 4ZM12 14a2 2 0 1 0 0-4 2 2 0 0 0 0 4ZM20.5 12a2 2 0 1 1-4 0 2 2 0 0 1 4 0Z"></path>
-              </svg>
-              {dropdownVisible && (
-                <div className="news-feed-comment-delete-edit-dropdown-menu-container">
-                  <div className="news-feed-comment-delete-edit-dropdown-menu">
-                    <div className="news-feed-comment-delete-edit-dropdown-item" onClick={handleEditClick}>
-                      Edit Comment
-                    </div>
-                    <div className="news-feed-comment-delete-edit-dropdown-item" onClick={handleDeleteClick}>
-                      Delete Comment
-                    </div>
-                  </div>
-                </div>
-              )}
-            </div>
-          </div>
         </div>
+      </div>
 
-
+      <PostCommentsStats post={post}/>
 
     </>
 
